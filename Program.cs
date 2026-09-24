7fc80cfb57712a366a2648685d8933f62f3b634c
ElNateG <167994734+ElNateG@users.noreply.github.com>
2026-09-24 11:59:48 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,28 +1,43 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+    {
+        options.AddDefaultPolicy( policity =>
+            {
+                policity
+                    .AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+            }
+        )
+    }
+)
+
 var app = builder.Build();
 
-app.MapGet("/", () =>
+app.UseCors()
+
+app.MapGet("/",() =>
 {
     return "API Polleria funcionando";
 });
 
-app.MapGet("/api/polleria", () =>
+app.MapGet("/api/polleria",() =>
 {
     return Results.Ok(new[]
     {
-        new
-        {
-            id = 1,
-            codigo = "P001",
-            nombre = "Pollo a la brasa"
+        new{
+            id=1,
+            codigo="P001",
+            nombre="Pollo a la brasa",
         },
-        new
-        {
-            id = 2,
-            codigo = "P002",
-            nombre = "Pollo broaster"
+        new{
+            id=2,
+            codigo="P002",
+            nombre="Pollo broaster",
         }
     });
 });
 
-app.Run();
+var port = Environment.GetEnvironmentVariable("Port")??"10000",
+app.Run($"https://0.0.0.0:(port)");
